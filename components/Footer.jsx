792c24448b04d355b665f5a823c148639fbafe2e
fahimnirjon <fahimnirjon21@gmail.com>
2024-10-06 00:12:47 +0600

    added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className='py-10 mt-20 text-center'>
         Copyright © 2024 All rights reserved.
         <br />
-        This website is made with ♡ by LightCode.
+        This website is made with ♡ by Fahim Nirjon.
     </div>
   )
 }
